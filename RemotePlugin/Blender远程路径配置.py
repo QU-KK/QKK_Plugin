f48f31a9a5a7410e2530139f99f0879f5866d272
QU-KK <qkk1021@gmail.com>
2026-07-16 23:45:54 +0800

    路径配置说明

--- RemotePlugin/Blender远程路径配置.py
+++ RemotePlugin/Blender远程路径配置.py
@@ -1,7 +1,8 @@
 # Blender远程路径
 https://QU-KK.github.io/Blender_Plugin/chajian/index.json
 
-https://github.com/QU-KK/QKK_Plugin/blob/main/%E8%BF%9C%E7%A8%8B%E6%8F%92%E4%BB%B6%E5%BA%93%E6%B5%8B%E8%AF%95/index.json
+https://QU-KK.github.io/QKK_Plugin/RemotePlugin/index.json
+
 
 # 把Blender.exe  拖入CMD  复制下面
  --command extension server-generate --repo-dir=C:/目录
