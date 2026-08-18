8ed3f506e75a2035b03792b55a92180a7eb4df6b
QU-KK <qkk1021@gmail.com>
2026-08-18 22:48:22 +0800

    更新

---
+++ BlenderPlugin/Blender_Script/01-必装/addons/按钮小工具pro/Python脚本/03-PBR检查/03-行-2-框-对齐/10-Mask_R--1.py
@@ -0,0 +1,7 @@
+import bpy
+#设置PBR通道
+bpy.context.scene.view_settings.view_transform = 'Standard'
+for mat in bpy.data.materials:
+    nodes = mat.node_tree.nodes.get('Shader')
+    if nodes:
+        mat.node_tree.nodes["Shader"].inputs['PBR通道    '].default_value = 'Mask_R'
