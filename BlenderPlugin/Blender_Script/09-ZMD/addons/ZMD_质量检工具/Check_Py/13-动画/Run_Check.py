0027dfd507d7621fe5961422c741307ce18277dd
QU-KK <qkk1021@gmail.com>
2026-08-18 22:55:41 +0800

    更新

--- BlenderPlugin/Blender_Script/09-ZMD/addons/ZMD_质量检工具/Check_Py/13-动画/Run_Check.py
+++ BlenderPlugin/Blender_Script/09-ZMD/addons/ZMD_质量检工具/Check_Py/13-动画/Run_Check.py
@@ -6,15 +6,15 @@ Description = '检查是否存在动画数据'
 Check_Data = [Check_Item_Name]
 
 #检查多边面
-for obj in selected_objects:
-    Number = 0
-    for poly in obj.data.polygons:
-        if len(poly.vertices) > 4:
-            Number += 1
-    if Number != 0:
-        description = '存在多边面=' + str(Number)
-        data = [obj.name,description]
-        Check_Data.append(data)
+#for obj in selected_objects:
+    #Number = 0
+    #for poly in obj.data.polygons:
+        #if len(poly.vertices) > 4:
+            #Number += 1
+    #if Number != 0:
+        #description = '存在多边面=' + str(Number)
+        #data = [obj.name,description]
+        #Check_Data.append(data)
 
 # 枚举
 icon = 'NODE_SOCKET_SHADER'
